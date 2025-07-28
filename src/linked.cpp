ab0f99bcb8975ce37ee0e4691f4755bfed52244a
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-07-28 01:14:01 -0300

    acess and append functionality

--- src/linked.cpp
+++ src/linked.cpp
@@ -1,5 +1,8 @@
+#include <exception>
 #include <iostream>
 #include "linked_l/ki.hpp"
+#include <vector>
+#include "linked_l/linked_list.hpp"
 
 struct tt {
     int first;
@@ -12,7 +15,32 @@ int main() {
     ki();
     tt tee;
 
+    std::vector<int>::iterator it;
+
+
     std::cout << tee.second << std::endl;
+
+    ds::LinkedList<int> list;
+    ds::LinkedList<double> trouble(22.2f);
+
+    try {
+        list[0];
+    } catch (std::exception& e) {
+
+        std::cout << e.what();
+    }
+
+    list.append(22);
+    std::cout << "append 22" << '\n';
+
+    list.append(2);
+    std::cout << "append 2" << '\n';
+    
+    list.append(10);
+    std::cout << "append 10" << '\n';
+
+    std::cout << "Trouble first element: "<< trouble[0] << std::endl;
+    
     return 0;
 
 }
--- src/linked_l/linked_list.hpp
+++ src/linked_l/linked_list.hpp
@@ -1,6 +1,14 @@
 #pragma once 
 
 #include <cstddef>
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+    //todo guard all functions for a empty list
+    //and make a iterator
+    //test everything
+    //
 
 namespace ds {
 
@@ -14,23 +22,137 @@ class LinkedList {
     Node* m_root;
     size_t m_len;
 
-public:
+    public:
     LinkedList() :
         m_root(nullptr), m_len(0) {}
 
-    LinkedList(Type new_data) :
+    LinkedList(const Type new_data) :
         m_root(new Node(new_data, nullptr)), m_len(1) {}
 
-    void append(Type& new_data);
-    void insert(size_t index);
-    Type remove(size_t index);
+    void append(const Type&& new_data) {
 
-    Type& operator[](size_t index);
+        Node* new_node = new Node{new_data, nullptr};
 
-    ~LinkedList();
+        Node* cur_node = m_root;
 
-private:
-        
+        if (cur_node == nullptr) {
+            m_root = new_node;
+            m_len++;
+            return;
+        }
+
+        while(cur_node->next != nullptr) {
+
+            cur_node = cur_node->next;
+
+        }
+
+        cur_node->next = new_node;
+        m_len++;
+        return;
+    }
+
+    void insert(const size_t index, const Type new_data) {
+
+        Node* new_node = new Node{new_data, nullptr};
+
+        if (index == 0) {
+            new_node->next = m_root;
+            m_root = new_node;
+            return;
+        } 
+
+        Node* prev_node = LinkedList<Type>::get(index - 1);
+
+        new_node->next = prev_node->next;
+        prev_node->next = new_node;
+        m_len++;
+        return;
+
+    }
+
+    Type remove(const size_t index){
+
+
+
+    }
+
+    Type pop() {
+
+        Type poped = last().data;
+
+        delete last();
+
+        get(m_len -2).next = nullptr;
+
+        m_len--;
+
+        return poped;
+
+    }
+
+    Type& operator[](const size_t index) {
+
+        if (index >= m_len) 
+            throw std::out_of_range("Error: the index " + std::to_string(index) + " is out of range for the list\n");
+
+        Node* cur_node = m_root;
+
+        for (size_t i = 0; i < index; ++i) {
+
+            cur_node = cur_node->next;
+        }
+
+        return cur_node->data;
+    }
+
+    ~LinkedList() {
+
+        Node* cur_node = m_root;
+        Node* prev_node = m_root;
+
+        while (cur_node != nullptr) {
+
+            prev_node = cur_node;
+            cur_node = cur_node->next;
+
+            delete prev_node;
+        }
+
+        //std::cout << "List freed\n";
+
+    }
+
+    private:
+
+    Node& last() {
+
+        Node* cur_node = m_root;
+
+        while (cur_node->next != nullptr) {
+
+            cur_node = cur_node->next;
+        }
+
+        return cur_node;
+    }
+
+
+    Node& get(const size_t index) {
+
+        if (index >= m_len) 
+            throw std::out_of_range("Error: the index " + std::to_string(index) + " is out of range for the list\n");
+
+        Node* cur_node = m_root;
+
+        for (size_t i = 0; i < index; ++i) {
+
+            cur_node = cur_node->next;
+        }
+
+        return cur_node;
+    }
+
 
 };
 
