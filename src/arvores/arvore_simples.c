4d490d749a0611ebe4b61eb68a6d8ac068ac4888
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-08-21 12:25:00 -0300

    print tree

--- src/arvores/arvore_simples.c
+++ src/arvores/arvore_simples.c
@@ -1,6 +1,11 @@
+#include <graphviz/gvcext.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <graphviz/cgraph.h>
+#include <graphviz/gvc.h>
+#include <string.h>
 
 typedef struct Node {
 
@@ -51,7 +56,7 @@ void insert_binary(Node* node, int input) {
 
 void print_order(Node* node) {
 
-    if (!node) return;
+    if (node == NULL) return;
 
     print_order(node->left);
 
@@ -85,9 +90,11 @@ void print_postorder(Node* node) {
 
 };
 
+
 Node* generate_tree(int* numbers, size_t size) {
 
     Node* root = malloc(sizeof(Node));
+    
     root->value = numbers[0];
     root->left = NULL;
     root->right = NULL;
@@ -100,6 +107,70 @@ Node* generate_tree(int* numbers, size_t size) {
 
     return root;
 }
+
+void tree_to_graph(Agraph_t* graph, Node* node) {
+
+    char buff[10];
+    sprintf(buff, "%d", node->value);
+    Agnode_t* parent = agnode(graph, buff, 1);
+
+    if (node->left != NULL) {
+
+        sprintf(buff, "%d", node->left->value);
+        Agnode_t* child_left = agnode(graph, buff, 1);
+
+        Agedge_t* edge = agedge(graph, parent, child_left, NULL, 1);
+
+        tree_to_graph(graph, node->left);
+
+    }
+    
+    if (node->right != NULL) {
+
+        sprintf(buff, "%d", node->right->value);
+        Agnode_t* child_right = agnode(graph, buff, 1);
+
+        Agedge_t* edge = agedge(graph, parent, child_right, NULL, 1);
+
+        tree_to_graph(graph, node->right);
+
+    }
+
+
+}
+
+void print_tree(Node* tree) {
+
+    Agraph_t* graph = agopen("Arvore", Agdirected, NULL);
+
+    tree_to_graph(graph, tree);
+
+    GVC_t* context = gvContext();
+
+    // Layout the graph
+    if (gvLayout(context, graph, "dot") != 0) {
+        fprintf(stderr, "Error: Failed to layout graph\n");
+        return ;
+    }
+    
+    // Render to PNG file
+    if (gvRenderFilename(context, graph, "png", "arvore_simples.png") != 0) {
+        fprintf(stderr, "Error: Failed to render graph to file\n");
+        return ;
+    }
+    
+    // Free layout data
+    gvFreeLayout(context, graph);
+    
+    // Close the graph
+    agclose(graph);
+    
+    // Free the context
+    gvFreeContext(context);
+    
+
+}
+
 
 int main() {
 
@@ -118,6 +189,8 @@ int main() {
     printf("Pre ordem: ");
     print_preorder(tree);
     printf("\n");
+
+    print_tree(tree);
 
     return 0;
 
