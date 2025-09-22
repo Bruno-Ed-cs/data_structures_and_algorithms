a16438a735b1c3ff1c55a83821caa1dd7ee692ce
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-09-22 10:35:05 -0300

    random list

--- src/ordenacao/testes.cpp
+++ src/ordenacao/testes.cpp
@@ -1,6 +1,8 @@
+#include <cstdlib>
 #include <iostream>
 #include <array>
 #include <functional>
+#include <random>
 #include <chrono>
 #include <format>
 #include <cassert>
@@ -10,6 +12,26 @@
 #include "insertion.hpp"
 #include "selection.hpp"
 #include "shell.hpp"
+
+//tempos de execução
+// time ./ordenadores random 100000
+// Bubble sort = { }
+// Time = 81345 milliseconds
+// 
+// Insertion sort = { }
+// Time = 47172 milliseconds
+// 
+// Selection sort = { }
+// Time = 41885 milliseconds
+// 
+// Shell sort = { }
+// Time = 73 milliseconds
+// 
+// 
+// real	2m50.504s
+// user	2m50.456s
+// sys	0m0.005s
+
 
 void print_sorting(const std::string&& sorting_name, std::function<void(std::vector<int>&)> sort_func, std::vector<int> target) {
 
@@ -21,15 +43,28 @@ void print_sorting(const std::string&& sorting_name, std::function<void(std::vec
 
     std::cout << sorting_name << " sort = { ";
 
-    for (int number : target) {
+    if (target.size() < 50) {
 
-        std::cout << number << " ";
+        for (int number : target) {
+
+            std::cout << number << " ";
+        }
+
+        std::cout << "}\n";
+
+        auto elapsed_time = end_time - start_time;
+
+        std::cout << "Time = " << elapsed_time.count() << " nanoseconds" << std::endl << std::endl;
+
+    } else {
+    
+        std::cout << "}\n";
+
+        auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+        std::cout << "Time = " << elapsed_time.count() << " milliseconds" << std::endl << std::endl;
+
     }
-
-    std::cout << "}\n";
-
-    auto elapsed_time = end_time - start_time;
-    std::cout << "Time = " << elapsed_time.count() << "ns" << std::endl << std::endl;
 
     //verifica se o algoritimo realmente oredenou o array
     std::vector<int> ideal_result = target;
@@ -46,6 +81,23 @@ int main(int argc, char** argv) {
     if (argc <= 1) {
 
         numeros = { 33, 10, 100, 34, 6, 3, 2, 7, 9, 345 };
+
+    } else if (std::string(argv[1]) == "random") {
+
+
+        int len = std::stoi(argv[2]);
+        numeros.reserve(len);
+
+        std::random_device rand;
+        std::mt19937 gen(rand());
+
+        std::uniform_int_distribution<int> int_range(0, 10000);
+
+        for (int i = 0; i < len; ++i) {
+
+            numeros.push_back(int_range(gen));
+        }
+
 
     } else {
 
