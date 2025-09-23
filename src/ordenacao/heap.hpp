8ae1b569da638b7b9159cbf401b89950ac5dec2a
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-09-23 14:04:52 -0300

    upgrade comentarios

--- src/ordenacao/heap.hpp
+++ src/ordenacao/heap.hpp
@@ -3,26 +3,29 @@
 
 namespace Order {
 
+//formulas para tratar o array como arvore
 int get_parent(int index) { return (index - 1) / 2; };
 int get_left(int index) { return 2 * index + 1; };
 int get_right(int index) { return 2 * index + 2; };
 
+//função para fazer um no atender a condição do max heap
 void heapify (std::vector<int>& list, int root, int limit) {
 
-    int left = get_left(root);
-    int right = get_right(root);
+    int left_index = get_left(root);
+    int right_index = get_right(root);
     int biggest = root;
 
 
-    if (left < limit && list[left] > list[biggest]) {
-        biggest = left;
+    if (left_index < limit && list[left_index] > list[biggest]) {
+        biggest = left_index;
     }
 
-    if (right < limit && list[right] > list[biggest]) {
-        biggest = right;
+    if (right_index < limit && list[right_index] > list[biggest]) {
+        biggest = right_index;
     }
 
 
+    //se o no não atende a condição entao aplicamos o balanceamento no maior elemento para fazer ele ir em direção a raiz
     if (biggest != root) {
 
         std::swap(list[root], list[biggest]);
@@ -43,6 +46,7 @@ void heap_sort(std::vector<int>& list) {
 
     }
 
+    // aqui fazemos de fato a ordenação pegando a raiz do heap
     for (int i = size -1; i > 0; --i) {
 
         std::swap(list[i], list[0]);
